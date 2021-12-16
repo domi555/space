6c3cbf276235b7b29af24aad665a3c8e621c667c
OnSebii <lang.sebastian.006@gmail.com>
2021-12-16 11:13:57 +0100

    Added Camera

--- client/src/views/Camera.vue
+++ client/src/views/Camera.vue
@@ -1,5 +1,12 @@
 <template>
   <v-container fluid class="pa-0" style="margin-top: 60px;">
+    <div class="">
+      <div class="a">
+        <br />
+        <p>Code: {{ code }}</p>
+        <v-quagga :onDetected="logIt" :readerSize="readerSize" :readerTypes="['ean_reader']"></v-quagga>
+      </div>
+    </div>
     <div class="d-flex justify-center align-center" style="position: fixed; bottom: 0; width: 100%;">
       <v-btn class="teal darken-2 white--text mb-4">Skip</v-btn>
     </div>
@@ -7,7 +14,29 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      readerSize: {
+        width: 640,
+        height: 480,
+      },
+      detecteds: [],
+      code: '',
+      readerTypes: ['code_128_reader', 'ean_reader', 'ean_8_reader', 'code_39_reader'],
+    };
+  },
+  methods: {
+    logIt(data) {
+      this.code = data.codeResult.code;
+      console.log('detected', data);
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.a {
+  padding-left: 620px;
+}
+</style>
