8ffcb0d4ba0084fc036837c2afd9110a3ad85117
OnSebii <lang.sebastian.006@gmail.com>
2022-02-03 12:31:14 +0100

    update

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool();
+// ToDo: maxpool size
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -3,6 +3,9 @@ const { loginUser, registerUser, logoutUser } = require('../controllers/sessions
 
 const router = express.Router();
 
+// Routen Absicherung funktioniert noch nicht da wir den Server und Client
+// getrennt betreiben und nicht so wie im Arbeitsblatt User-Sessions von der 4 Klasse
+
 // const redirectLogin = (req, res, next) => {
 //   if (!req.session.userId) res.status(400).send('You are not logged in!');
 //   else next();
